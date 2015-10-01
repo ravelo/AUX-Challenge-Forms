99e70b7715d90afd492070a8de1833071729f42c
Tricia Ravelo <triciaravelo@matt.local>
2015-10-01 16:36:18 -0400

    cc validator revised

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,39 +1,42 @@
-// Credit card – Adds class to show clearer validation for js users
-$('.cc-number-control .icon-form-validation').addClass('force-show-invalid');
+$(function () {
 
+	var cardNumberValidate = $('.cc-number-control .icon-form-validation'),
+		cardTypeButtons_all = $('.cc-type-control input[type=radio]'),
+		cardCodeImg = $('.cc-code-sprite');
 
-// Credit card validate
-$('#cc-number').validateCreditCard(function(result) {
-
-	var radioButtons_all = $('.cc-type-control input');
-
-	// Check card type – No card type detected
-	if (result.card_type == null) {
-
-		radioButtons_all.prop('checked', false);
-
-	// Check card type – Amex stuff
-	} else if (result.card_type.name == 'amex') {
-
-		$('#amex').prop('checked', true);
-		$('.cc-code-sprite').addClass('is-amex');
-
-	// Check card type – All card types but amex
-	} else {
-
-		var cardName = result.card_type.name;
-		var radioButton = $('#' + cardName);
-		radioButton.prop('checked', true);
-		$('.cc-code-sprite').removeClass('is-amex');
-	} 
-
-
-	// Validate – Indicate if card number is valid
-	if (result.valid == true ) {
-		$('.cc-number-control .icon-form-validation').removeClass('only-show-invalid');
-	} else {
-		$('.cc-number-control .icon-form-validation').addClass('only-show-invalid');
+	function changeIfAmex() {
+		if ($('#amex').is(':checked')) {
+			cardCodeImg.addClass('is-amex');
+		} else {
+			cardCodeImg.removeClass('is-amex');
+		}
 	}
 
+	// Credit card – Adds class to show clearer validation for js users
+	cardNumberValidate.addClass('force-show-invalid');
 
-}, { accept: ['visa', 'amex', 'mastercard', 'discover'] });
+	// Look to see if Amex is manually checked
+	cardTypeButtons_all.change(changeIfAmex);
+
+	// CARD VALIDATION (using plugin)
+	$('#cc-number').validateCreditCard(function(result) {
+
+		// Check card type – No card type detected
+		if (result.card_type === null) {
+			cardTypeButtons_all.prop('checked', false);
+		} else {
+			var cardTypeButton = $('#' + result.card_type.name);
+			cardTypeButton.prop('checked', true);
+			changeIfAmex();
+		}
+
+		// Validate – Indicate if card number is valid
+		if (result.valid === true ) {
+			cardNumberValidate.removeClass('only-show-invalid');
+		} else {
+			cardNumberValidate.addClass('only-show-invalid');
+		}
+
+	}, { accept: ['visa', 'amex', 'mastercard', 'discover'] });
+
+}); // END doc ready
